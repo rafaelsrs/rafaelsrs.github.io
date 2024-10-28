f35827c8c8659e135341c8d2623fbcba2d8b594a
RafaelSRS <79536608+rafaelsrs@users.noreply.github.com>
2024-10-28 20:20:36 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :departamentos
   get "produtos/busca", to: "produtos#busca", as: :busca_produto
   resources :produtos, only: [:new, :create, :destroy, :edit, :update]
+  get "produtos", to: "produtos#index"
   root to: "produtos#index"
 end
